1d4d4b39dcc25f0d7d891c9ca166c45a69c69f48
Vitaliy <tkachuk.vitaliy.1995@gmail.com>
2020-05-24 13:30:00 +0300

    Express config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
-const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
+const httpPort: number = parseInt(process.env.HTTP_PORT) || 8080;
 
 const initHttpServer = (myHttpPort: number) => {
-    const app = express();
+    const app: express.Application = express();
     app.use(bodyParser.json());
 
-    app.get('/check', (request, response) => {
-        response.send('Check success');
+    app.get('/check', (req, res) => {
+        res.send('Check success');
+    });
+
+    app.listen(myHttpPort, () => {
+        console.log(`Application listening on port ${ myHttpPort }`);
     });
 };
 
